aaecd217768af091ff369603f8ee2c3a8f180085
DharunThota <dharunth@gmail.com>
2025-03-04 22:31:07 +0530

    add cookies

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 import express from "express";
-import axios from "axios";
 import cors from "cors";
 import bodyParser from "body-parser";
 import statusMonitor from "express-status-monitor";
+import cookieParser from "cookie-parser";
 
 import authRouter from "./routers/auth.js";
 import healthRouter from "./routers/health.js";
 import registerRouter from "./routers/register.js";
-import metricRouter from "./routers/metric.js"
-import listsRouter from "./routers/lists.js"
 
 import serverlessExpress from "aws-serverless-express";
 
 
 const app = express();
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 const ref = new Date(1/1/1970);
-export const domain = "https://srg.social";
+export const domain = "http://localhost:3001";
+const algo = "hot";
 
 //middlewares
 app.use(statusMonitor());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({ origin: domain, credentials: true }));
 app.use(bodyParser.json());
 
 /*override endpoints below here*/
@@ -31,8 +31,6 @@ app.use(bodyParser.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/health", healthRouter);
 app.use("/api/v1/register", registerRouter);
-app.use("/api/v1/metric", metricRouter);
-app.use("/api/v1/lists", listsRouter);
 
 const server = serverlessExpress.createServer(app);
 
